2df54ce8b488e1189485d0ee17354488f7d31542
onlyJinx <925198583@qq.com>
2017-09-17 12:02:11 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,15 @@
-213213213131213213213213213
+###搬瓦工默认禁用epel
+yum remove epel-release -y
+yum install epel-release -y
+
+###yum install gcc gettext autoconf libtool automake make pcre-devel asciidoc xmlto c-ares-devel libev-devel libsodium-devel mbedtls-devel -y
+yum install gcc gettext autoconf libtool automake make pcre-devel wget git vim asciidoc xmlto c-ares-devel libev-devel -y
+###手动编译libsodium-devel mbedtls-devel
+
+wget https://tls.mbed.org/download/mbedtls-2.6.0-gpl.tgz
+tar xvf mbedtls-2.6.0-gpl.tgz
+cd mbedtls-2.6.0
+make SHARED=1 CFLAGS=-fPIC
+sudo make DESTDIR=/usr install
+popd
+sudo ldconfig
