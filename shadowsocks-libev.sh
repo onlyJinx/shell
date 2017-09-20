30706b4074c734163b4c5de3efcc85af182ae84e
onlyJinx <925198583@qq.com>
2017-09-20 11:20:05 +0800

    Update shadowsocks-libev.sh

--- shadowsocks-libev.sh
+++ shadowsocks-libev.sh
@@ -80,6 +80,15 @@ User=root
 WantedBy=multi-user.target
 EOF
 
+###firewall oprt
+sed -i '$d' /etc/firewalld/zones/public.xml
+echo "  <port protocol=\"udp\" port=\"443\"/>" >> /etc/firewalld/zones/public.xml
+echo "  <port protocol=\"tcp\" port=\"443\"/>" >> /etc/firewalld/zones/public.xml
+echo "  <port protocol=\"udp\" port=\"28532\"/>" >> /etc/firewalld/zones/public.xml
+echo "  <port protocol=\"tcp\" port=\"28532\"/>" >> /etc/firewalld/zones/public.xml
+echo "</zone>" >> /etc/firewalld/zones/public.xml
+firewall-cmd --reload > null
+
 systemctl start ssl&&systemctl enable ssl
 ### remove the file
 cd /root && rm -fr mbedtls-2.6.0 shadowsocks-libev libsodium-1.0.13 simple-obfs test.sh mbedtls-2.6.0-gpl.tgz libsodium-1.0.13.tar.gz
