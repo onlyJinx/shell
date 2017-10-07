04f09bff615d8e60ca23c2e61fa60f207cc415f6
onlyjinx <925198583@qq.com>
2017-10-07 16:00:31 +0800

    update this

--- Unity.js
+++ Unity.js
@@ -1,33 +1,31 @@
 
-var bodyE = null;
-var child = null;
-var fDiv = null;
-var oUl = null;
-var fUl = null;
+var bodyE,child,fDiv,oUl,fUl;
 window.onload=function(){
 	bodyE = document.getElementsByTagName('body')[0];                                                                                            //获取body
 	fDiv = document.getElementsByTagName('div')[0];                                                                                                    //获取第一个DIV,用于 插入悬浮窗（文件管理窗口）
-                     addMF();                                                                                                                                                                            //在状态栏添加一个“filemanager”入口
+                     addMF();                                                                                                                                                                           //在状态栏添加一个“filemanager”入口
 	child = document.createElement('div');                                                                                                                         //创建遮罩层，也是用于承载整个文件管理窗口的
 	var fra = document.createElement('div');                                                                                                                      //创建件管理窗口
 	document.getElementById("filema").onclick=function(){                                                                                           //为filemanager添加监听事件，整个JS的入口
                                 addEE();
-				bodyE.className="ng-scope overflow1";
+                                bodyE.className="ng-scope overflow1";
                                 oUl = document.getElementById('gul');
-                                getData();
+                                ajaxHttp(0,null,oUl);
+                                ajaxHttp(2,null,document.getElementById('freeSp'));
+                                
 	};
 	function addEE(){                                                                                                                                                              //从服务器获取文件列表
 		fDiv.style.zIndex = '-10';
 		child.id = 'download';
 		child.style.overflowX = 'auto';
 		child.className='overlay';
-		fra.innerHTML='<div class="modal-dialog"><div class="modal-content"><div class="modal-header ng-scope"><button class="close" onclick="remove()">×</button><h4 class="ng-binding">File Lists</h4></div><form class="modal-body ng-pristine ng-valid ng-scope"><fieldset><ul id="gul" style="padding:10px;"></ul><br><br></fieldset><div class="modal-footer"><button type="button" class="btn btn-default ng-binding" onclick="remove()">Cancel</button><button type="button" class="btn btn-default ng-binding btn-primary" onclick="clearAll()">ClearAll</button></div></form></div></div>';
+		fra.innerHTML='<div class="modal-dialog text-center"><div class="modal-content"><div  class="modal-header ng-scope"><button class="close" onclick="remove()">×</button><h3 id="freeSp"></h3></div><form class="modal-body ng-pristine ng-valid ng-scope"><fieldset><ul id="gul" style="padding:10px;"></ul></fieldset><div class="modal-footer"><button type="button" class="btn btn-default ng-binding" onclick="remove()">Cancel</button><button type="button" class="btn btn-default ng-binding btn-primary" onclick="clearAll()">ClearAll</button></div></form></div></div>';
 		child.appendChild(fra);
 		bodyE.appendChild(child);
 	}
 	
 
-}
+};
 
 function remove(){
 	fDiv.style.zIndex = '1000';
@@ -44,43 +42,69 @@ function addMF(){                                                               
     newul.innerHTML = '<li><a href="#" dropdown-toggle>FilesManager <span class=""></span></a></li>';
     par.appendChild(newul);
 }
-
+/*
 function getData(){                                                                                                                                                                               //向服务器请求文件列表
             var xmlHttp = new XMLHttpRequest();
-	    xmlHttp.timeout = 0;
-            xmlHttp.open('get','/change.jsp',true);
+            xmlHttp.open('get','change.jsp',true);
             xmlHttp.onreadystatechange=function (){
                                 if(xmlHttp.readyState==4){
                                     //oUl.innerHTML = xmlHttp.responseText;
-				    if(xmlHttp.responseText.indexOf('<h3>')==2){
-					oUl.innerHTML = xmlHttp.responseText;
-				    }else{
-					getData();
-					//alert(xmlHttp.responseText);
-				    };
+                                if(xmlHttp.responseText.indexOf('<h3>')==2){
+                                    oUl.innerHTML = xmlHttp.responseText;
+                                }else{
+                                    getData();
+                                    //alert(xmlHttp.responseText);
+                                };
                                 }
             };
             xmlHttp.send();
-	    xmlHttp.ontimeout = function(){alert("timeout!!!");};
+}
+*/
+
+function deleFile(pa,a){
+   ajaxHttp(1,pa,'',false);                                                                                                                                                                        //向服务器发起删除请求
+   a.parentNode.parentNode.parentNode.removeChild(a.parentNode.parentNode);                                                                  //删除TR标签
+   ajaxHttp(2,null,document.getElementById('freeSp'));                                                                                                                    //重新获取可用内存
+   if(oUl.getElementsByTagName('tr').length===0){                                                                                                                         //判断是否为最后一个文件
+        oUl.innerHTML = '<h4>There is no file can be deleted</h4>';
+    }
 }
 
 function clearAll(){                                                                                                                                                                                
-        singledel(0,'/web_home/downloads/');
+        ajaxHttp(1,0);                                                                                                                                                                                //向服务器发起删除请求
+        ajaxHttp(2,null,document.getElementById('freeSp'));                                                                                                              //重新获取可用内存
+        oUl.innerHTML = '<h4>AllFile Deleted</h4>';                                                                                                                         //清空整个ul列表
 }
 
-function singledel(meth,path){                                                                                                                                                           //meth参数：0--全部清除；1--删除一个文件，path传递一个具体文件夹路劲
-  var meth = '?meth='+meth;                                                                                                                                                              //3--向服务器请求文件列表（等合并两个JSP后再实现）   
-  var path = '&delfil='+path;
-  path = encodeURI(encodeURI(path));                                                                                                                                              //两次编码（服务器要一次解码），处理中文乱码问题
-  var xmlH = new XMLHttpRequest();
-  xmlH.open('GET','this.jsp'+meth+path,true);
-  xmlH.onreadystatechange=function(){
-            if(xmlH.readyState==4){
-                     //alert(xmlH.responseText);
-                    getData();                                                                                                                                                                          //刷新客户端文件列表（后期改用为parent.removeChild(node),减少客户端/服务器工作量）
-            }
-  };
-   xmlH.send();
+/**
+ * meth参数：
+ * 0--获取文件列表；
+ * 1--删除文件，path传递一个具体文件夹路劲或传递数字“0”，代表全部删除
+ * 2--获取可用内存
+ * */
+
+function ajaxHttp(meth,path,innerObj,sync){
+        if(sync===undefined){
+                sync = true;
+        }
+        var meth = '?meth='+meth;
+        var path = '&delfil='+path;
+        path = encodeURI(encodeURI(path));                                                                                                                                        //两次编码（服务器要一次解码），处理中文乱码问题
+        var xmlH = new XMLHttpRequest();
+        xmlH.open('GET','this.jsp'+meth+path,sync);                                                                                                                            //同步处理，防止查询可用内存是文件还未被删除导致获取的数据有误
+        xmlH.onreadystatechange=function(){
+                  if(xmlH.readyState===4){
+                      if(innerObj===undefined){
+                          return;
+                      }else{
+                          innerObj.innerHTML = xmlH.responseText;
+                      }
+                  }
+        };
+         xmlH.send();
+         xmlH.ontimeout=xmlH.onerror=function(){
+                   alert('request timeout!');
+         };
  }
  
 
--- this.jsp
+++ this.jsp
@@ -1,19 +1,71 @@
 
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
-<%@page import="java.io.*,java.net.*" %>
+<%@page import="java.io.*,java.net.*,java.util.*" %>
 
 <%  
         String spath = request.getParameter("delfil");
         spath = java.net.URLDecoder.decode(spath, "UTF-8");
         int meth = Integer.parseInt(request.getParameter("meth"));
-        File path = new File(spath);
         if(meth==1){
-            singleDelete(path);
+                if(spath.equals("0")){
+                     clearAll(new File("C:/Users/RUO/Desktop/css"));
+                }else{
+                    File path = new File(spath);
+                    singleDelete(path);
+                }
+                
+        }else if(meth==0){
+                if(start().length != 0){
+                        out.println("<table border=\"1\">");
+                        for(String se : start()){
+                                se = se.replace("\\", "/");                                                                                                                       //避开转义字符---反斜杠
+                                //out.println("<tr><td class=\"private_td\"><a href='"+se.replace("/web_home","")+"'>"+se.replace("/web_home/downloads/","")+" </a></td><td style=\"white-space:nowrap;\" class=\"private_td\"><a href='javascript:void(0);' onclick='ajaxHttp(\"1\",\""+se+"\")'>Delete</a></td></tr>");
+                                out.println("<tr><td class=\"private_td\"><a href='"+se.replace("/web_home","")+"'>"+se.replace("/web_home/downloads/","")+" </a></td><td style=\"white-space:nowrap;\" class=\"private_td\"><a href='javascript:void(0);' onclick='deleFile(\""+se+"\",this)'>Delete</a></td></tr>");
+                        }
+                        out.println("</table>");
+                }else{
+                        out.println("<h4>NotFound File</h4>");
+                }
         }else{
-            clearAll(path);
+            out.println("<h3>FREESPACE : "+Roots()+" MB</h3>");
         };
         
         
+%>
+
+<% //查询文件列表%>
+<%!
+    //String path = "C:/Users/RUO/Desktop/css";
+    File files = new File("C:/Users/RUO/Desktop/css");
+    String[] ss;
+    ArrayList<String> arr = new ArrayList();
+    public void parseFile(File file){
+        File[] f = file.listFiles();
+        if(f!=null){                                                                                                                                                    //防止空指针错误
+            for(File ff : f){
+                if(ff.isDirectory()){
+                    parseFile(ff);
+                }else{
+                    if(ff.getName().substring(ff.getName().lastIndexOf(".") + 1).equals("mht") | ff.getName().substring(ff.getName().lastIndexOf(".") + 1).equals("chm") | ff.getName().substring(ff.getName().lastIndexOf(".") + 1).equals("url") | ff.getName().substring(ff.getName().lastIndexOf(".") + 1).equals("html") | ff.getName().substring(ff.getName().lastIndexOf(".") + 1).equals("txt")){
+                            ff.delete();
+                    }else{
+                            arr.add(ff.toString());
+                    }
+                }
+            }
+        }else{
+            return;
+        }
+    }
+
+    public String[] start(){
+        arr.clear();                                                                                                                                                  //重置arraylist列表，避免无限打印
+        parseFile(files);
+        ss = arr.toArray(new String[arr.size()]);
+        return ss;
+    }
+    
+
 %>
 
 <%!
@@ -34,6 +86,8 @@
         };
 %>
 
+
+<% //全部清理%>
 <%!
         public void clearAll(File file){
                 File[] files = file.listFiles();
@@ -48,4 +102,11 @@
                     }
                 }
         }
+%>
+<% //查可用储存空间 %>
+<%!
+	public long Roots(){
+		File sto = new File("C:/");
+		return sto.getFreeSpace()/(long)1024/(long)1024;
+	}
 %>
