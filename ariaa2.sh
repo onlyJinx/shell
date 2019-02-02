acee27e31aa2b6a9670569613e9ad183fe4c70a8
onlyJinx <925198583@qq.com>
2019-02-02 14:35:44 +0800

    1.34.0

--- ariaa2.sh
+++ ariaa2.sh
@@ -1,6 +1,6 @@
 cd ~
 yum install -y gcc-c++ bison libssh2-devel expat-devel gmp-devel nettle-devel libssh2-devel zlib-devel c-ares-devel gnutls-devel libgcrypt-devel libxml2-devel sqlite-devel gettext lzma-devel xz-devel gperftools gperftools-devel gperftools-libs jemalloc-devel
-wget https://github.com/aria2/aria2/releases/download/release-1.33.1/aria2-1.33.1.tar.gz
+wget https://github.com/aria2/aria2/releases/download/release-1.34.0/aria2-1.34.0.tar.gz
 tar zxvf aria2*
 cd aria2*
 ./configure
