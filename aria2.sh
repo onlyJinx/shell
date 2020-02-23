1e1d985abec54e748d40926d3d500dc314864cdd
onlyJinx <925198583@qq.com>
2020-02-23 16:58:23 +0800

    Update aria2.sh

--- aria2.sh
+++ aria2.sh
@@ -1,21 +1,21 @@
 cd ~
 yum install -y gcc-c++ bison libssh2-devel expat-devel gmp-devel nettle-devel libssh2-devel zlib-devel c-ares-devel gnutls-devel libgcrypt-devel libxml2-devel sqlite-devel gettext lzma-devel xz-devel gperftools gperftools-devel gperftools-libs jemalloc-devel
-wget https://github.com/aria2/aria2/releases/download/release-1.34.0/aria2-1.34.0.tar.gz
+wget https://github.com/aria2/aria2/releases/download/release-1.35.0/aria2-1.35.0.tar.gz
 tar zxvf aria2*
 cd aria2*
 ./configure
 make&make install
 
-mkdir /web_home
-mkdir /web_home/downloads
-mv aria2.conf /
+mkdir /etc/aria2
+#mkdir /web_home/downloads
+#mv aria2.conf /
 
 cat >/etc/systemd/system/aria2.service<< EOF
 [Unit]
 Description=aria2c
 After=network.target
 [Service]
-ExecStart=/usr/local/bin/aria2c --conf-path=/aria2.conf
+ExecStart=/usr/local/bin/aria2c --conf-path=/etc/aria2.conf
 User=root
 [Install]
 WantedBy=multi-user.target
